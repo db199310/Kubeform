6b422212bba8c97349847f87fd808e0da7a2e11a
Azure Pipelines <sedp-ci@shell.com>
2021-03-31 16:09:44 +0000

    [skip ci] [kubeform] Update Terraform modules

--- data/config_data.go
+++ data/config_data.go
@@ -20,15 +20,6 @@ package data
 
 var ModuleConfig = []Config{
 	{
-		"SDPAzadfv1", "git::https://sede-ds-adp.visualstudio.com/Platform%20-%20General/_git/sedp-tf-az-datafactories?ref=v0.1.0", "azurerm", "", "",
-	},
-	{
-		"SDPAzsbv1", "git::https://sede-ds-adp.visualstudio.com/Platform%20-%20General/_git/sedp-tf-az-servicebus?ref=v0.2.1", "azurerm", "", "",
-	},
-	{
-		"SDPAzFnv1", "git::https://sede-ds-adp.visualstudio.com/Platform%20-%20General/_git/sedp-tf-az-fnapp?ref=v0.4.3", "azurerm", "", "",
-	},
-	{
 		"SDPAzAppv1", "git::https://sede-ds-adp.visualstudio.com/Platform%20-%20General/_git/sedp-tf-az-appservice?ref=v0.3.0", "azurerm", "", "",
 	},
 	{
@@ -36,5 +27,14 @@ var ModuleConfig = []Config{
 	},
 	{
 		"SDPAzSqlv1", "git::https://sede-ds-adp.visualstudio.com/Platform%20-%20General/_git/sedp-tf-az-sqldb?ref=v0.3.1", "azurerm", "", "",
+	},
+	{
+		"SDPAzadfv1", "git::https://sede-ds-adp.visualstudio.com/Platform%20-%20General/_git/sedp-tf-az-datafactories?ref=v0.1.0", "azurerm", "", "",
+	},
+	{
+		"SDPAzsbv1", "git::https://sede-ds-adp.visualstudio.com/Platform%20-%20General/_git/sedp-tf-az-servicebus?ref=v0.2.1", "azurerm", "", "",
+	},
+	{
+		"SDPAzFnv1", "git::https://sede-ds-adp.visualstudio.com/Platform%20-%20General/_git/sedp-tf-az-fnapp?ref=v0.4.3", "azurerm", "", "",
 	},
 }
