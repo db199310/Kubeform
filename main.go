75e33f7408a0a2175f9c32495a16cf923e8184e1
Amol Chauhan <amol.chauhan@shell.com>
2021-04-16 09:33:16 +0000

    importing databricks labs provider

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm"
+	"github.com/databrickslabs/terraform-provider-databricks/provider"
 )
 
 var licenseHeaderFile string
